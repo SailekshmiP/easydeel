f60473696de017f98df00d77a595e0aacd30d306
SailekshmiP <sailekshmip@gmail.com>
2020-11-28 16:41:06 +0530

    changes updated validation messages

--- src/app/home/pincodes/add-pincodes/add-pincodes.component.ts
+++ src/app/home/pincodes/add-pincodes/add-pincodes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pincodes',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPincodesComponent implements OnInit {
 
-  constructor() { }
+  pincodeFormRegistration:FormGroup;
+  submitted = false;
+  
+ pincode;
+  // cimage;
+  // des;  
+  // mtype="";
+  // mctype="";
+  // mstyle="";
+  
+  constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit() {
-  }
+    this.pincodeFormRegistration = this.formbuilder.group(
+      {
+        pincode: ['', Validators.required],
+        // cimage:['', Validators.required],
+        // des: ['', Validators.required],
+        // mtype: ['', Validators.required],
+        // mctype: ['', Validators.required],
+        // mstyle: ['', Validators.required],
+    })
 
+  }
+get f() { return this.pincodeFormRegistration.controls; }
+
+  submit(){
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.pincodeFormRegistration.invalid) {
+        return;
+    }
+    else{
+
+    }
+  }
 }
